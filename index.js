b6d502fc97551c29dfeeca5b84153eec8d5f9d48
Dedan Opiyo <dedan.opiyo@student.moringaschool.com>
2025-04-01 18:39:32 +0300

    destructuring strings, arrays, and objects

--- index.js
+++ index.js
@@ -28,21 +28,40 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+//const animalSounds = farmAnimals.split(' ');
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+console.log(moo);
+
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const [bessie, ,dolly, babe, little] = farmAnimals.split(' ');
+console.log(bessie, dolly, babe, little);
+console.log('farmAnimals', farmAnimals);
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const [blackAndWhite, black, pink] = [bessie, dolly, babe];
+console.log('expexting bessie...:', blackAndWhite, black, pink);
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const [red, orange, yellow, green, blue, indigo, violet] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+console.log(red);
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+const [r, o, y, g, b, , v] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const [ , , , , , indg, ] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+console.log(indg);
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+const {muppetName, color, song, job, partner} = muppet;
+console.log(muppetName);
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const {song2, song4} = nestedMuppet.album.theMuppetMovie;
+const {nestedJob, nestedPartner} = nestedMuppet;
+console.log('nestedJob', nestedJob)
